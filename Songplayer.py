47a114b8657a933202f84219b8292407a2e826bc
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2021-07-20 03:46:20 -0400

    Update Songplayer.py

--- Songplayer.py
+++ Songplayer.py
@@ -1,1 +1,18 @@
+from tkinter.constants import N
+from gi.repository import Notify
+from playsound import *
+import tkinter as tk 
+import random
+
+usr = input("What is your Username?")
+
+songnumbers = int(input("You should have your songs labeled by number in assending order. \n How many songs do you have?"))
+while True:
+    songnumber = random.randint(1,songnumbers)
+    Notify.init("Jammer")
+    Notify.Notification.new("New song Playing!").show()
+
+    str(songnumbers)
+
+    playsound('/home/'+ usr +'/Music/' + str(songnumbers) + '.mp3')
 
